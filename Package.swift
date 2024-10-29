545096c1d9d446ff3f5fc94d3c685eb8432df72b
mhayes853 <matthewha853@gmail.com>
2024-10-29 03:21:54 -0700

    Remove HummingbirdAuth

--- Package.swift
+++ Package.swift
@@ -15,15 +15,13 @@ let package = Package(
     .package(url: "https://github.com/swift-server/async-http-client", from: "1.23.1"),
     .package(url: "https://github.com/vapor/jwt-kit", from: "5.1.0"),
     .package(url: "https://github.com/vapor/sqlite-nio", from: "1.10.3"),
-    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.3.3"),
-    .package(url: "https://github.com/hummingbird-project/hummingbird-auth", from: "2.0.0-rc.5")
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.3.3")
   ],
   targets: [
     .target(
       name: "RudeusServer",
       dependencies: [
         .product(name: "Hummingbird", package: "hummingbird"),
-        .product(name: "HummingbirdAuth", package: "hummingbird-auth"),
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
         .product(name: "WPHaptics", package: "WhyPeopleKit"),
         .product(name: "WPFoundation", package: "WhyPeopleKit"),
