98fc9c75310373a997e0ce6e0c7301c69af16d7a
mhayes853 <matthewha853@gmail.com>
2024-10-29 11:56:54 -0700

    Cleanup routes

--- Sources/RudeusServer/Hummingbird/RudeusApplication.swift
+++ Sources/RudeusServer/Hummingbird/RudeusApplication.swift
@@ -20,34 +20,9 @@ extension RudeusApplication: ApplicationProtocol {
     let router = Router(context: RudeusRequestContext.self)
     router.add(middleware: RudeusAuthenticationMiddleware(keys: self.environment.keys))
       .group("api")
-      .post("register") { request, context in
-        let request = try await request.decode(as: RudeusRegisterRequest.self, context: context)
-        let user = try await self.environment.database.createUser(named: request.name)
-        let token = try await self.environment.keys.sign(user)
-        return EditedResponse(
-          status: .created,
-          response: RudeusRegisterResponse(id: user.id, name: user.name, token: token)
-        )
-      }
-      .post("pattern") { request, context in
-        let user = try context.requireUser()
-        let request = try await request.decode(as: RudeusSavePatternRequest.self, context: context)
-        let pattern = RudeusPattern(
-          name: request.name,
-          user: user,
-          ahapPattern: request.ahapPattern,
-          platform: request.platform
-        )
-        try await self.environment.database.save(pattern: pattern)
-        try await self.environment.slackClient.send(
-          message: .patternShared(channelId: self.environment.slackChannelId, pattern)
-        )
-        return EditedResponse(status: .created, response: pattern)
-      }
-      .get("pattern") { _, _ in
-        let patterns = try await self.environment.database.patterns()
-        return EditedResponse(status: .ok, response: RudeusPatternsResponse(patterns: patterns))
-      }
+      .post("register") { try await self.register(request: $0, context: $1) }
+      .post("pattern") { try await self.editPattern(request: $0, context: $1) }
+      .get("pattern") { _, _ in try await self.patterns() }
     return router.buildResponder()
   }
 
@@ -56,5 +31,54 @@ extension RudeusApplication: ApplicationProtocol {
       address: .hostname(self.environment.host, port: self.environment.port),
       serverName: "Rudeus"
     )
+  }
+}
+
+// MARK: - Register
+
+extension RudeusApplication {
+  private func register(
+    request: Request,
+    context: Context
+  ) async throws -> EditedResponse<RudeusRegisterResponse> {
+    let request = try await request.decode(as: RudeusRegisterRequest.self, context: context)
+    let user = try await self.environment.database.createUser(named: request.name)
+    let token = try await self.environment.keys.sign(user)
+    return EditedResponse(
+      status: .created,
+      response: RudeusRegisterResponse(id: user.id, name: user.name, token: token)
+    )
+  }
+}
+
+// MARK: - Edit Pattern
+
+extension RudeusApplication {
+  private func editPattern(
+    request: Request,
+    context: Context
+  ) async throws -> EditedResponse<RudeusPattern> {
+    let user = try context.requireUser()
+    let request = try await request.decode(as: RudeusSavePatternRequest.self, context: context)
+    let pattern = RudeusPattern(
+      name: request.name,
+      user: user,
+      ahapPattern: request.ahapPattern,
+      platform: request.platform
+    )
+    try await self.environment.database.save(pattern: pattern)
+    try await self.environment.slackClient.send(
+      message: .patternShared(channelId: self.environment.slackChannelId, pattern)
+    )
+    return EditedResponse(status: .created, response: pattern)
+  }
+}
+
+// MARK: - Patterns
+
+extension RudeusApplication {
+  private func patterns() async throws -> EditedResponse<RudeusPatternsResponse> {
+    let patterns = try await self.environment.database.patterns()
+    return EditedResponse(status: .ok, response: RudeusPatternsResponse(patterns: patterns))
   }
 }
